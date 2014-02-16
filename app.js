05d3b5376e9db41e7d70434651deab7f610650d5
Changz <destiny54706@gmail.com>
2014-02-16 16:32:46 +0900

    name added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
  * Module dependencies.
  */
 
+//I'm Changju hahahahahahahahahahahahahahahaha
+
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
